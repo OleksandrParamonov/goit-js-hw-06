0e8ce266f8d16fbb6b96465c7539c18d3993191f
OleksandrParamonov <sanya.paramonov97@gmail.com>
2023-03-15 22:22:40 +0200

    first change

---
+++ src/js/task-01.js
@@ -0,0 +1,6 @@
+const categoriesRef = document.querySelector('ul#categories')
+const itemRef = categoriesRef.querySelectorAll('li')
+const titleRef = categoriesRef.querySelectorAll('h2')
+console.log(titleRef)
+
+
