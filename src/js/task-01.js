aaf8a230ec93c94ebac7313d86970a8322689a27
OleksandrParamonov <107406898+OleksandrParamonov@users.noreply.github.com>
2023-03-22 11:06:51 +0200

    task1-done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,12 @@
-const categoriesRef = document.querySelector('ul#categories')
-const itemRef = categoriesRef.querySelectorAll('li')
-const titleRef = categoriesRef.querySelectorAll('h2')
-console.log(titleRef)
+const categoriesRef = document.querySelector('ul#categories');
+const itemRef = categoriesRef.querySelectorAll('.item')
+console.log(`Number of categories: ${itemRef.length}`)
 
+itemRef.forEach(item => {
+    const titleRef = item.querySelector('h2').textContent;
+    const elementsRef = item.querySelectorAll('li').length
+
+    console.log(`Category: ${titleRef}`);
+    console.log(`Elements: ${elementsRef}`);
+})
 
