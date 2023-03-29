d451bbd93ec4ac7c3a8a74c18e98da90db5cce81
OleksandrParamonov <107406898+OleksandrParamonov@users.noreply.github.com>
2023-03-29 21:38:25 +0300

    task-8_done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const formRef = document.querySelector('form.login-form');
+const ollInputRef = document.querySelectorAll('input')
 
+
+formRef.addEventListener('submit', onSubmitForm)
+function onSubmitForm(event) {
+    event.preventDefault()
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+    
+      if (email.value === "" || password.value === "") {
+
+        return alert ("Please fill in all the fields!");
+      }
+
+      event.currentTarget.reset()
+  }
