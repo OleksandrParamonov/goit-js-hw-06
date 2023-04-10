417a6d6458a12e775982904367bb3a601611bc6e
OleksandrParamonov <107406898+OleksandrParamonov@users.noreply.github.com>
2023-04-10 20:34:04 +0300

    task-9_done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const colorRef = document.querySelector('.color');
+const buttonRef = document.querySelector('.change-color');
+
+buttonRef.addEventListener('click', onButtonColorChange)
+
+function onButtonColorChange (){
+  const color = getRandomHexColor();
+  bodyRef.style.backgroundColor = color;
+  colorRef.textContent = color;
+ 
+}
+
