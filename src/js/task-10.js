3bf34cc41f5a67fe037f1d914e528be6b0ad38a4
OleksandrParamonov <107406898+OleksandrParamonov@users.noreply.github.com>
2023-04-11 11:39:51 +0300

    task-10_notDone

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const inputRef = document.querySelector('input')
+const controlsRef = document.querySelector('#controls');
+const createRef = document.querySelector('[data-create]');
+const destroyRef = document.querySelector('[data-destroy]');
+const boxesRef = document.querySelector('div#boxes')
+console.log(destroyRef);
+
+function createBoxes(amount) {
+  amount.forEach(newAmount => {
+  let newBox = document.createElement('div');
+  newBox.style.width = `30px`;
+  newBox.style.height = `30px`;
+  const backgroundColor = getRandomHexColor();
+  backgroundColor = newBox.style.backgroundColor
+createBoxes.push(newAmount)
+boxesRef.append(...createBoxes)
+console.log(createBoxes)
+});
+}
