c698d5eac89a24612cf1db1aa33729d1c3d627a5
OleksandrParamonov <107406898+OleksandrParamonov@users.noreply.github.com>
2023-04-12 12:59:39 +0300

    task10-done

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,22 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const inputRef = document.querySelector('input')
-const controlsRef = document.querySelector('#controls');
+const inputRef = document.querySelector('input');
 const createRef = document.querySelector('[data-create]');
 const destroyRef = document.querySelector('[data-destroy]');
-const boxesRef = document.querySelector('div#boxes')
-console.log(destroyRef);
+const boxesRef = document.querySelector('div#boxes');
 
-function createBoxes(amount) {
-  amount.forEach(newAmount => {
-  let newBox = document.createElement('div');
-  newBox.style.width = `30px`;
-  newBox.style.height = `30px`;
-  const backgroundColor = getRandomHexColor();
-  backgroundColor = newBox.style.backgroundColor
-createBoxes.push(newAmount)
-boxesRef.append(...createBoxes)
-console.log(createBoxes)
-});
+// Додаємо слухача події на кнопку Створити
+createRef.addEventListener('click', createBoxes);
+
+// Додаємо слухача події на кнопку Очистити
+destroyRef.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(inputRef.value)
+  for (let i = 0; i < amount; i+=1) {
+    const newBox = document.createElement('div');
+    newBox.style.width = `${30 + i * 10}px`;
+    newBox.style.height = `${30 + i * 10}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+    boxesRef.appendChild(newBox);
+  }
+  
+}
+
+function destroyBoxes() {
+  boxesRef.innerHTML = '';
 }
